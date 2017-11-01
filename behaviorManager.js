9ce085b46b1e61e5b9c60183fd7b045a286c93b1
Sebastian <sklingler@pier1.com>
2017-11-01 16:46:59 -0500

    Improve behaviorPool

--- behaviorManager.js
+++ behaviorManager.js
@@ -2,19 +2,31 @@ const behaviorManager = function (dispatcher) {
 
 	const platforms = {};
 
+	// Returns a getter for behaviors on a given platform (use for nested behaviors)
+	const behaviorPool = (platform, state, action, callingBehavior) => behaviorName => {
+		// if (currentBehavior === behaviorName) {
+		// 	return () => console.log('Cannot')
+
+		// }
+		if (platforms[platform][behaviorName]) {
+			let behavior = platforms[platform][behaviorName];
+			return () => behavior(state, action, behaviorPool(platform, state, action));
+		} else {
+			return () => console.log('Behavior not found');
+		}
+	}
+
 	// Pass the behavior pool into the behavior and log details
-	const wrapBehavior = (platform, behaviorName, behavior, actionType) => {
+	const wrapBehavior = (platform, behaviorName, behavior) => {
 		return function (state, action) {
-			let behaviorPool = platforms[platform];
-			actionType = actionType || 'internal';
-			console.log('Platform:', platform, 'Behavior:', behaviorName, 'Trigger:', actionType);
-			return behavior(state, action, behaviorPool);
+			console.log('Platform:', platform, 'Behavior:', behaviorName, 'Trigger:', action.type);
+			return behavior(state, action, behaviorPool(platform, state, action));
 		}
 	}
 
 	// Action type is optional, will link the behavior to a state change
 	const register = (platform, behaviorName, behavior, actionType) => {
-		behavior = wrapBehavior(platform, behaviorName, behavior, actionType);
+		behavior = wrapBehavior(platform, behaviorName, behavior);
 
 		platforms[platform] = platforms[platform] || {};
 		platforms[platform][behaviorName] = behavior;
--- index.js
+++ index.js
@@ -2,22 +2,38 @@ const store = require('./store');
 const { CART } = require('./actionTypes');
 const { cartAddItem } = require('./actions');
 
+
+
 // We register our behaviors like so:
-store.register('adobe', 'my_behavior', () => console.log('yay'));
-
-// Adding the fourth parameter links this behavior to a state change:
-store.register('adobe', 'cart_add', (state, action, behaviors) => {
-
-	// In here, we have access to the state of the digital data layer, 
-	// the action itself,
-	// and all registered behaviors for the current platform
+store.register('adobe', 'my_behavior', (state, action, behaviorPool) => {
+	console.log('My first behavior - Yay');
 
 	console.log(state);
 	console.log(action);
-	console.log(behaviors);
 
-	behaviors['my_behavior']();
+	// behaviorPool('cart_add')();
+});
 
+// Adding the fourth parameter links this behavior to a state change:
+store.register('adobe', 'cart_add', (state, action, behaviorPool) => {
+
+	// In here, we have access to the state of the digital data layer, 
+	// the action itself,
+	// and the behavior pool for the current platform
+
+	console.log(state);
+	console.log(action);
+
+	// Existing behaviors can be triggered like so:
+	behaviorPool('my_behavior')();
+
+	// Non existing behaviors will log:
+	behaviorPool('non_existent')();
+
+}, CART.ADD);
+
+store.register('google', 'card_add', () => {
+	console.log('google fired');
 }, CART.ADD);
 
 store.dispatch(cartAddItem('test'));
